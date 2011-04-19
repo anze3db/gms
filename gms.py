605ef31fc4483d326656bb7b9bdcc53608b036fa
Smotko <ensmotko@gmail.com>
2011-04-19 11:31:18 +0200

    TI now record mouse position between right mouse down and up (while super key is pressed)

--- gms.py
+++ gms.py
@@ -6,27 +6,45 @@ Created on Apr 11, 2011
 '''
 from sys import exit
 from Xlib import display
+import thread
 
 superPressed = False
 rightMousePressed = False
  
 def OnKeyDown(event):
+    global superPressed
     if event.Key == 'x':
         exit()
     if event.Key == 'Super_L':
         superPressed = True
-    
+        
 def OnKeyUp(event):
+    global superPressed
     if event.Key == 'Super_L':
         superPressed = False
- 
+        
 def OnMouseDown(event):
+    global rightMousePressed
+    global superPressed
+    
     if event.MessageName == 'mouse right down':
         rightMousePressed = True
-
+        
+        if superPressed:
+            thread.start_new_thread(record_mouse_pos, ())
+        
 def OnMoseUp(event):
     if event.MessageName == 'mouse right up':
+        global rightMousePressed
         rightMousePressed = False
+        
+def record_mouse_pos():
+    gesture = []
+    global rightMousePressed
+    while rightMousePressed:
+        gesture.append(mousepos())
+    print gesture
+        
 
 def mousepos():
     """mousepos() --> (x, y) get the mouse coordinates on the screen (linux, Xlib)."""
