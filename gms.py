9ff3abf2855292c56a621015fa5991259308dc43
Smotko <ensmotko@gmail.com>
2011-04-19 10:55:45 +0200

    Added checking if super key & right mouse are pressed

--- gms.py
+++ gms.py
@@ -2,26 +2,36 @@
 '''
 Created on Apr 11, 2011
 
-@author: smotko
+@authors: Anze, Matic, Miha
 '''
-import Xlib
-import Xlib.display
-import time
-import sys
+from sys import exit
+from Xlib import display
+
+superPressed = False
+rightMousePressed = False
  
 def OnKeyDown(event):
     if event.Key == 'x':
-        sys.exit()
-    print event
+        exit()
+    if event.Key == 'Super_L':
+        superPressed = True
     
 def OnKeyUp(event):
-    print event
+    if event.Key == 'Super_L':
+        superPressed = False
  
 def OnMouseDown(event):
-    print event
+    if event.MessageName == 'mouse right down':
+        rightMousePressed = True
 
 def OnMoseUp(event):
-    print event.MessageName, event.Position, event.Window, event.WindowName, event.WindowProcName
+    if event.MessageName == 'mouse right up':
+        rightMousePressed = False
+
+def mousepos():
+    """mousepos() --> (x, y) get the mouse coordinates on the screen (linux, Xlib)."""
+    data = display.Display().screen().root.query_pointer()._data
+    return data["root_x"], data["root_y"]
 
 # EVENT: 'MessageName', 'Position', 'Window', 'WindowName', 'WindowProcName'
 
