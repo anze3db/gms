dcc967f37938bf8940346d449a2e07e63f70d560
HairyFotr <maticpotocnik@gmail.com>
2011-04-20 10:58:25 +0200

    sendkeys popen

--- sendkeys.py
+++ sendkeys.py
@@ -2,14 +2,11 @@ import Xlib
 import Xlib.display
 import time
 import os
+import subprocess
 
 # xsendkeys is in ubuntu package lineakd
-os.system('gnome-terminal')
-time.sleep(1)
-os.system('xsendkeys l+s+Return')
-time.sleep(3)
-os.system('wmctrl -r gnome-terminal -b toggle,hidden')
+subprocess.Popen(['gnome-terminal'])
 time.sleep(2)
-os.system('wmctrl -a gnome-terminal')
+subprocess.Popen(['xsendkeys','l+s+Return'])
 time.sleep(3)
 os.system('xsendkeys Alt+F4')
