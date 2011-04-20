cc3dabfb446c68d3245d537a9327a84366d1de3e
HairyFotr <maticpotocnik@gmail.com>
2011-04-20 10:16:01 +0200

    demo sendkeys

--- sendkeys.py
+++ sendkeys.py
@@ -4,4 +4,12 @@ import time
 import os
 
 # xsendkeys is in ubuntu package lineakd
+os.system('gnome-terminal')
+time.sleep(1)
+os.system('xsendkeys l+s+Return')
+time.sleep(3)
+os.system('wmctrl -r gnome-terminal -b toggle,hidden')
+time.sleep(2)
+os.system('wmctrl -a gnome-terminal')
+time.sleep(3)
 os.system('xsendkeys Alt+F4')
